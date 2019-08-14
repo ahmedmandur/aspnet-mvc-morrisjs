0f3e9279bedb519007a52d6de99edd9736a8206a
Ahmed Mandour <ahmedmandur@gmail.com>
2019-08-14 12:10:15 +0300

    add line chart demo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AspNetMvcMorrisCharts.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly Random Getrandom = new Random();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult GetData(int count)
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+            Entry[] data = new Entry[count];
+            for (int i = 0; i < count; i++)
+            {
+
+                int year = DateTime.Now.AddYears(i * -1).Year;
+                data[i] = new Entry
+                {
+                    value = GetRandomNumber(i * 30, year),
+                    year = year.ToString()
+                };
+            }
+
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (Getrandom) // synchronize
+            {
+                return Getrandom.Next(min, max);
+            }
+        }
+
+        public class Entry
+        {
+            public string year { get; set; }
+            public int value { get; set; }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿
+# Asp.net MVC 5 with Morrisjs Charts
+Dynamically render morrisjs charts with ajax calls inside asp.net mvc 5 controllers
+
+![Line chart](https://i.imgur.com/QBYly17.png)
